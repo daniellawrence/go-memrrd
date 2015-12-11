461c6588dc552b1b362b2e576f889abe3f620f48
Daniel Lawrence <dannyla@linux.com>
2015-12-10 22:29:57 -0800

    Cleaned up interface + tests

--- memrrd.go
+++ memrrd.go
@@ -1,15 +1,33 @@
 package memrrd
 
+import (
+	"fmt"
+	"encoding/json"
+)
 type RRD struct {
-	name  string
-	value [10]int
+	Name  string     `json:"name"`
+	Value []float64  `json:"value"`
 }
 
-func (self *RRD) New(name string)  {
-	self.name = name
+func (self *RRD) Add(new_value interface{}) {
+	copy(self.Value[1:], self.Value[0:])
+	if x, ok := new_value.(float64); ok {
+		self.Value[0] = x
+		return
+	}
 }
 
-func (self *RRD) Add(new_value int) {
-	copy(self.value[1:], self.value[0:])
-	self.value[0] = new_value
+func (self *RRD) Json() string {
+	b, err := json.Marshal(self)
+	if err != nil {
+		return "{\"error\": \"Failed to convert RRD to json\"}"
+	}
+	return string(b)
+}
+
+func main() {
+	r := RRD{
+		Name:   "1",
+		Value: []float64{1.0}}
+	fmt.Println(r.Json())
 }
--- memrrd_test.go
+++ memrrd_test.go
@@ -5,31 +5,36 @@ import (
 )
 
 func Test_memrrd_set_name(t *testing.T) {
-	rrd := RRD{}
-	rrd.New("Example")
-	// example_rrd.Add(1)
-	if rrd.name != "Example" {
+	rrd := RRD{
+		Name: "test",
+	}
+	if rrd.Name != "test" {
 		t.Error("Failed to set name")
 	}
 }
 
 func Test_memrrd_default_is_empty(t *testing.T) {
-	rrd := RRD{}
-	rrd.New("Example")
-	if rrd.value != [10]int{0,0,0,0,0,0,0,0,0,0} {
-		t.Error("Failed to init value")
+	rrd := RRD{
+		Name: "test",
+		Value: []float64{1.0}}
+	expected := "{\"name\":\"test\",\"value\":[1]}"
+
+	if rrd.Json() != expected {
+		t.Error("Matching problem")
+		t.Error(expected)
+		t.Error(rrd.Json())
 	}
 }
 
 func Test_memrrd_add1(t *testing.T) {
-	rrd := RRD{}
-	rrd.New("Example")
-	rrd.Add(1)
-	if rrd.value != [10]int{1,0,0,0,0,0,0,0,0,0} {
-		t.Error("Failed to add value")
-	}
-	rrd.Add(2)
-	if rrd.value != [10]int{2,1,0,0,0,0,0,0,0,0} {
-		t.Error("Failed to add value")
+	rrd := RRD{
+		Name: "test",
+		Value: make([]float64, 3)}
+	expected := "{\"name\":\"test\",\"value\":[0,0,0]}"
+
+	if rrd.Json() != expected {
+		t.Error("Matching problem")
+		t.Error(expected)
+		t.Error(rrd.Json())
 	}
 }
