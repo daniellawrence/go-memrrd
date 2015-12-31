cf8f3783f8c73a6f1a50f0ebb7792e41a97ac3cc
Daniel Lawrence <dannyla@linux.com>
2015-12-31 12:51:19 +1100

    Trying to add ticks to the RRD

--- memrrd.go
+++ memrrd.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"encoding/json"
+	"time"
 )
+
 type RRD struct {
 	Name  string     `json:"name"`
 	Value []float64  `json:"value"`
+	Lock  bool       `json:"lock"`
 }
 
 func (self *RRD) Add(new_value float64) {
@@ -22,10 +25,27 @@ func (self *RRD) Json() string {
 	return string(b)
 }
 
+func tickAdd(r *RRD) {
+	t := time.Now()
+	now_epoch_int64 := t.Unix()
+	now_epoch := float64(now_epoch_int64)
+	fmt.Printf("Trying to add %d\n", now_epoch)
+	for {
+		if ! r.Lock {
+			r.Lock = true
+			r.Add(now_epoch)
+			r.Lock = false
+			break
+		}
+		fmt.Println("Failed to add new epoch")
+	}
+
+}
+
 func main() {
 	r := RRD{
 		Name:   "name",
-		Value: []float64{1.0, 2.0}}
-	r.Add(3.0)
+		Value: []float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0}}
+	go tickAdd(&r)
 	fmt.Println(r.Json())
 }
